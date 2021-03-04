a10ba79f96b241d3a4e5a065b1ea7e8a0c7d686c
Trinity Chung <trinityjchung@gmail.com>
2021-03-04 14:02:13 -0800

    put aar files back

---
+++ ftc-app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/opmodes/test/WheelClicksTest.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.teleop.opmodes.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.math.LCHSMath;
+import org.firstinspires.ftc.teamcode.teleop.opmodes.drive.BaseDrive;
+import org.firstinspires.ftc.teamcode.teleop.utility.Button;
+import org.firstinspires.ftc.teamcode.teleop.utility.TeleOpBase;
+
+@TeleOp(group="Test")
+//@Disabled
+public class WheelClicksTest extends BaseDrive {
+
+    private final Button resetButton = new Button();
+
+    @Override
+    protected void initialize() {
+        robot.driveTrain.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        robot.driveTrain.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        updateTelemetry();
+    }
+
+    @Override
+    protected void update() {
+        updateResetButton();
+        updateTelemetry();
+    }
+
+    private void updateResetButton() {
+        resetButton.update(gamepad1.a);
+        if (resetButton.is(Button.State.DOWN)) {
+            robot.driveTrain.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        }
+    }
+
+    private void updateTelemetry() {
+        double lf = robot.driveTrain.lf.getCurrentPosition();
+        double rf = robot.driveTrain.rf.getCurrentPosition();
+        double lb = robot.driveTrain.lb.getCurrentPosition();
+        double rb = robot.driveTrain.rb.getCurrentPosition();
+        telemetry.addData("lf", lf);
+        telemetry.addData("rf", rf);
+        telemetry.addData("lb", lb);
+        telemetry.addData("rb", rb);
+        telemetry.addData("avg", (lf + rf + lb + rb) / 4.0 );
+        telemetry.addData("------", "controls");
+        telemetry.addData("reset clicks", "a");
+        telemetry.update();
+    }
+
+}
