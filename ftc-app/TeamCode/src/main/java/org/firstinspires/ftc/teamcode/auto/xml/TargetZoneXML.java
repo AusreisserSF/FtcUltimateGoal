4c771f47131c29d6956da20362789b8fc700004b
AusreisserSF <45668270+AusreisserSF@users.noreply.github.com>
2021-03-25 01:31:24 -0700

    Fixed small bug in TargetZoneXML.java

--- ftc-app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/xml/TargetZoneXML.java
+++ ftc-app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/xml/TargetZoneXML.java
@@ -70,10 +70,10 @@ public class TargetZoneXML {
             xpath = xpathFactory.newXPath();
 
             // Disallow nesting of "EXECUTE_TARGET_ZONE_STEPS"
-            String commandPath = "//command[@id=" + "'" + "EXECUTE_TARGET_ZONE_STEPS" + "']";
-            Node commandNode = (Node) xpath.evaluate(commandPath, document, XPathConstants.NODE);
-            if (commandNode != null)
-                throw new AutonomousRobotException(TAG, "Command EXECUTE_TARGET_ZONE_STEPS not allowed");
+            String executePath = "//EXECUTE_TARGET_ZONE_STEPS"; // anywhere in the document
+            Node targetZonesNode = (Node) xpath.evaluate(executePath, document, XPathConstants.NODE);
+            if (targetZonesNode != null)
+                throw new AutonomousRobotException(TAG, "Nested EXECUTE_TARGET_ZONE_STEPS not allowed");              throw new AutonomousRobotException(TAG, "Command EXECUTE_TARGET_ZONE_STEPS not allowed");
 
         } catch (ParserConfigurationException pex) {
             throw new AutonomousRobotException(TAG, "DOM parser Exception " + pex.getMessage());
